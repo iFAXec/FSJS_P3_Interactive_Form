543c8782929152723539dc0a143586ebfd0eec93
ifaxec <xecfahim.ahmed@gmail.com>
2022-10-09 18:12:36 +0100

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-console.log('test');
+const pageLoad = document.getElementById("name");
+const otherJobRole = document.getElementById("other-job-role");
+const selectJobRole = document.getElementById("title");
+const valueJobRole = document.querySelector("option"[value]);
+console.log(valueJobRole);
+
+
+
+
+
+
+//name field has focus when the page loads
+function windowLoad(){
+    pageLoad.focus();
+}
+windowLoad();
+
+//hide other field under job role when form loads
+function hideOtherField(){
+    otherJobRole.style.display = "none";
+}
+hideOtherField(); 
+
+
+function hideOrDisplayOther(){  
+
+    for (let i = 0; i < valueJobRole.length; i++) {
+
+        if (valueJobRole[i] === "other") {
+            otherJobRole.style.display = "block";    
+        } else {
+            otherJobRole.style.display = "none";    
+    }    
+        
+    }
+        
+};
+
+console.log(hideOrDisplayOther());
+
+selectJobRole.addEventListener("change", hideOrDisplayOther);
+ 
+
+
+
+
+
+
+
