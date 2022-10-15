b018f764e605a2953cf24a658c13bd3c2090b990
ifaxec <xecfahim.ahmed@gmail.com>
2022-10-15 22:38:24 +0100

    updated script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@ function windowLoad(){
 }
 windowLoad();
 
+
+//<------Job Role-------->
 //hide other field under job role when form loads
 function hideOtherField(){
     otherJobRole.style.display = "none";
@@ -28,14 +30,13 @@ function hideOrDisplayOther(){
                 otherJobRole.style.display = "none";    
         }    
 };
-
 //console.log(hideOrDisplayOther());
-
 selectJobRole.addEventListener("change", hideOrDisplayOther);
  
 
+//<------T-Shirt Info -------->
 const designSelect = document.getElementById("design");
-//console.log(designSelect);
+console.log(designSelect);
 
 const shirtColorOption = document.querySelectorAll("#color option");
 const shirtSelect = document.getElementById("color")
@@ -44,8 +45,9 @@ const shirtSelect = document.getElementById("color")
 shirtSelect.disabled = true;
 
 designSelect.addEventListener("change", e=>{
-        shirtSelect.enabled = true;
-        //const shirtCollection = shirtSelect.children;
+        shirtSelect.disabled = false;
+        const shirtCollection = shirtSelect.children;
+        //console.log(shirtCollection);
     for (let i = 1; i < shirtColorOption.length; i++){
     const designSelectValue = e.target.value;   
     const shirtTheme = shirtColorOption[i].getAttribute("data-theme");
@@ -53,11 +55,11 @@ designSelect.addEventListener("change", e=>{
     //console.log(shirtTheme);
 
     if(designSelectValue === shirtTheme){
-        shirtColorOption[i].hidden = "false";
-        shirtColorOption[i].setAttribute("selected", "true")
+        shirtColorOption[i].hidden = false;
+        shirtColorOption[i].setAttribute("selected", true)
     }else{
-        shirtColorOption[i].hidden = "true";
-        shirtColorOption[i].setAttribute("selected", "false");
+        shirtColorOption[i].hidden = true;
+        shirtColorOption[i].setAttribute("selected", false);
     }
 }
 });
