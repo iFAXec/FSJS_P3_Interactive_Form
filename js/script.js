d0282627da1441b8f57ffbbf295b6a585367a6e8
ifaxec <xecfahim.ahmed@gmail.com>
2022-10-22 19:12:18 +0100

    updated script.js

--- js/script.js
+++ js/script.js
@@ -66,6 +66,8 @@ designSelect.addEventListener("change", e=>{
 
 //<-------------------register for Activities----------->
 
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+//console.log(checkboxes);
 const activitiesField = document.querySelector(".activities");
 //console.log(activitiesField);
 let total = 0;
@@ -73,9 +75,13 @@ let total = 0;
 activitiesField.addEventListener("change", (e)=>{
 
     const clicked = e.target;
-    const clickedDataCost = clicked.getAttribute("data-cost");
+    console.log(clicked);
+    const clickedDataCost = clicked.getAttribute("data-cost");    
     const clickValue = parseInt(clickedDataCost);
     const totalAmount = document.querySelector(".activities-cost");
+    const clickedDayAndTime = clicked.getAttribute("data-day-and-time");
+    console.log(clickedDayAndTime);
+  
 
      if(clicked.checked){
         total += clickValue;
@@ -83,13 +89,49 @@ activitiesField.addEventListener("change", (e)=>{
       }else{
        total -= clickValue;
        //console.log(total);
-    }  
+    };  
+
 totalAmount.innerHTML = total;
-}
-);
 
+//Loop through all the checkboxes to get the date ad time attribute and compare with click attribute
+for(let i = 0; i < checkboxes.length; i++){
+    const checkboxDayAndTime = checkboxes[i].getAttribute("data-day-and-time");
+    console.log(checkboxDayAndTime);
+    if(clickedDayAndTime === checkboxDayAndTime && clickedDayAndTime !== checkboxes[i]){
+        if(clicked.checked){
+            checkboxes[i].disabled = true;
+        }else{
+        checkboxes[i].disabled = false;
+    }
+}}});
 
+//<-----Payment info----->
+ const selectPayment = document.querySelector(".payment-methods");
+console.log(selectPayment);
 
+//Make credit card as a default drop down item
+const paymentOptions = document.getElementById("payment")[1];
+//console.log(paymentOptions);
+paymentOptions.selected = "true";
 
+selectPayment.addEventListener("change", (e)=>{
+    const creditCardInfo = document.getElementById("credit-card");
+    const paypalInfo = document.getElementById("paypal");
+    const bitcoinInfo = document.getElementById("bitcoin");
+    const paymentValue = e.target.value;
+    //console.log(paymentValue);
 
-
+    if(paymentValue === "paypal"){
+        paypalInfo.hidden = false;
+        creditCardInfo.hidden = true;
+        bitcoinInfo.hidden = true;
+    }else if (paymentValue === "bitcoin"){
+        bitcoinInfo.hidden = false;
+        creditCardInfo.hidden = true;
+        paypalInfo.hidden = true;
+    } else{
+        creditCardInfo.hidden = false;
+        paypalInfo.hidden= true;
+        bitcoinInfo.hidden = true;
+    }    
+});
